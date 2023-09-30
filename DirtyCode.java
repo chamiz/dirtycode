3a7bf80e0bd6d2a7e3a7862f3d5346839e6a7dc3
chamiz <chamila.ambahera@gmail.com>
2023-09-30 02:05:34 +0200

    Added Gradle support

--- DirtyCode.java
+++
@@ -1,5 +0,0 @@
-class DirtyCode{
-public static void main(String[] args){int a= 1;int b=2;int result=0;
-for(int i=1;i<=b;i++){result = a + i;}
-if(result>=3){System.out.println("Result is greater than or equal to 3");} else {System.out.println("Result is less than 3");}
-}}
