df6e0bf6a10d2af83bd06387cc7987b9ddb2ad42
chamiz <chamila.ambahera@gmail.com>
2023-09-30 22:40:52 +0200

    Added Grade wrapper

--- src/main/java/org/example/DirtyCode.java
+++ src/main/java/org/example/DirtyCode.java
@@ -1,5 +1,26 @@
-class DirtyCode{
-    public static void main(String[] args){int a= 1;int b=2;int result=0;
-        for(int i=1;i<=b;i++){result = a + i;}
-        if(result>=3){System.out.println("Result is greater than or equal to 3");} else {System.out.println("Result is less than 3");}
-    }}
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DirtyCode {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++)
+            numbers.add(i);
+        System.out.println(numbers.get(5));
+        try {
+            int result = divide(10, 0);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed.");
+        }
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0)
+            return 0;
+        else
+            return a / b;
+    }
+}
